999d8b7635a21ff93cc980e93314673d72579f79
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-09-06 11:56:26 -0300

    ss

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 export default function Layout() {
   return (
     <main className="flex h-[100dvh] w-full items-center justify-center bg-dark-1">
-      <div className="flex h-full max-w-[430px] flex-col justify-center overflow-hidden border bg-dark-1 px-6">
+      <div className="flex h-full w-full max-w-[430px] flex-col justify-center overflow-hidden border bg-dark-1 px-6">
         <Outlet />
       </div>
     </main>
