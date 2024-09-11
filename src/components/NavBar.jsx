5dd3949b2e252afad966b225b41fc6846e0ffdbd
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-09-11 20:02:37 -0300

    .

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import BackButton from "@/components/buttons/BackButton"
 
 export default function NavBar() {
   return (
-    <nav className="sticky left-0 right-0 top-0 z-50 flex h-16 w-full items-center justify-start bg-dark-1 py-5">
+    <nav className="sticky left-0 right-0 top-0 z-50 flex h-12 w-full items-center justify-start bg-dark-1 py-5">
       <BackButton />
     </nav>
   )
