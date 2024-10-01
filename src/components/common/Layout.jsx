7dd1abf50fa209510048c7bbe3a1f5938511d127
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-10-01 17:32:20 -0300

    changes in navbar (sticked to fixed) to avoid chrome bug

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -4,9 +4,9 @@ import NavBar from "./navbar/NavBar"
 export default function Layout() {
   return (
     <main className="flex h-auto w-full items-center justify-center bg-dark-1">
-      <div className="relative flex h-auto min-h-[100dvh] w-full max-w-[430px] flex-col gap-5 bg-dark-1 px-6 pb-6">
+      <div className="relative flex h-auto min-h-[100vh] w-full max-w-[430px] flex-col bg-dark-1">
         <NavBar />
-        <div className="mt-0 h-full">
+        <div className="mt-20 h-full px-6 pb-6">
           <Outlet />
         </div>
       </div>
--- src/components/common/navbar/NavBar.jsx
+++ src/components/common/navbar/NavBar.jsx
@@ -7,14 +7,16 @@ export default function NavBar() {
   const currentConfig = navbarConfig[pathname] || {}
 
   return (
-    <nav className="sticky left-0 right-0 top-0 z-50 flex h-12 w-full items-center justify-between bg-dark-1 py-8">
-      <div className="flex items-center gap-2">
-        <BackButton />
-        {currentConfig.title && (
-          <h1 className="text-heading-4">{currentConfig.title}</h1>
-        )}
-      </div>
-      {currentConfig.rightContent}
-    </nav>
+    <div className="fixed left-0 right-0 top-0 z-50 flex w-full justify-center bg-dark-1">
+      <nav className="flex h-16 w-full max-w-[430px] items-center justify-between px-6">
+        <div className="flex items-center gap-2">
+          <BackButton />
+          {currentConfig.title && (
+            <h1 className="text-heading-4">{currentConfig.title}</h1>
+          )}
+        </div>
+        {currentConfig.rightContent}
+      </nav>
+    </div>
   )
 }
